b78c2d486f11c33163d1ea95d7f3f2c28e657737
odin-coder <87134828+Odin-Coder@users.noreply.github.com>
2021-10-28 18:03:16 +0100

    set up main class

---
+++ classes.py
@@ -0,0 +1,12 @@
+class budget_class():
+    def __init__(self, balance):
+        self.balance = balance
+
+    def __repr__(self):
+        return f'The remaining balance is {self.balance}'
+
+    def withdraw(self, balance):
+        pass
+
+    def deposit(self):
+        pass
