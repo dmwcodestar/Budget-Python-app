92c233f5caa39a0725c104c8b6453faa37f65276
odin-coder <87134828+Odin-Coder@users.noreply.github.com>
2021-10-28 18:06:51 +0100

    app.py pushed to development

---
+++ app.py
@@ -0,0 +1,9 @@
+from classes import budget_class
+
+food = budget_class(200)
+print(food)
+print(food.balance)
+entertainment = budget_class(200)
+essential_bills = budget_class(600)
+print(entertainment)
+print(essential_bills)
